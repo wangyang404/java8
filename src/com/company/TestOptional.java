7be6c19c4dd03a0e5ae45a597a75358566f28174
wangyang404 <31433001+wangyang404@users.noreply.github.com>
2018-06-22 17:20:14 +0800

    test

--- src/com/company/TestOptional.java
+++ src/com/company/TestOptional.java
@@ -22,7 +22,8 @@ public class TestOptional {
     public void test1(){
         Optional<Employee> optional = Optional.of(null);
         Employee employee = optional.get();
-        System.out.println(employee);
+        System.out.println(employee + "testtest");
+
 
 
     }
